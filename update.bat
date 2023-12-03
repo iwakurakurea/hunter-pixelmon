c0d8ccdbfeb8b911ff5eb11a2c4d51529c985526
Colorblind22 <143967407+iwakurakurea@users.noreply.github.com>
2023-12-02 20:40:28 -0600

    randompatches mixin config change

--- update.bat
+++ update.bat
@@ -1,1 +1,11 @@
+echo OFF
 move .\update\* %APPDATA%\.minecraft\mods
+del %APPDATA%\.minecraft\mods\Project_MMO-1.16.5-3.69.0.jar
+move .\config\* %APPDATA%\.minecraft\config
+
+echo !! IMPORTANT:
+echo !! Line 284 of .minecraft/config/randompatches.toml should be:
+echo !! mixin_blacklist = ["ServerPlayNetHandlerKeepAlive"]
+echo !! If your game is crashing, please double check that.
+echo Update should now be installed. Love, Clair ♥
+pause
