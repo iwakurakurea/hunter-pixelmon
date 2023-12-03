1fad14b44dbc8f55176e63f11d4e72d8f2275cdc
Colorblind22 <143967407+iwakurakurea@users.noreply.github.com>
2023-12-02 21:05:12 -0600

    Update installgame.bat

--- installgame.bat
+++ installgame.bat
@@ -1,9 +1,16 @@
+echo OFF
+echo A forge installer window will appear after you press enter.
+echo Select  * Install Client  and press OK, leave everything else default.
+pause
+
 java -jar forge-1.16.5-36.2.34-installer.jar
 cd .\shared
 curl.exe -o Pixelmon-1.16.5-9.1.9-universal.jar https://mediafilez.forgecdn.net/files/4819/879/Pixelmon-1.16.5-9.1.9-universal.jar
 move .\* %APPDATA%\.minecraft\mods\
 cd ..\client
 move .\* %APPDATA%\.minecraft\mods\
+cd ..\config
+move .\* %APPDATA%\.minecraft\config\
 
-echo If you are seeing this message, the modpack should now be installed. Yippee. Love, Clair <3
+echo The modpack should now be installed. Yippee. Love, Clair ♥
 pause
