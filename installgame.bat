025869b3e76c2a7b4be3dbf908a25a06d4e95292
Colorblind22 <143967407+iwakurakurea@users.noreply.github.com>
2024-07-02 17:52:54 -0500

    Update installgame.bat

--- installgame.bat
+++ installgame.bat
@@ -4,6 +4,10 @@ echo Select  * Install Client  and press OK, leave everything else default.
 pause
 
 java -jar forge-1.16.5-36.2.34-installer.jar
+cls
+
+echo The script will now download the Pixelmon mod, after it is done it will install the mods for you. Please leave this window open.
+
 cd .\shared
 curl.exe -o Pixelmon-1.16.5-9.1.9-universal.jar https://mediafilez.forgecdn.net/files/4819/879/Pixelmon-1.16.5-9.1.9-universal.jar
 move .\* %APPDATA%\.minecraft\mods\
